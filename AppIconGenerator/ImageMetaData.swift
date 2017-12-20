e2b947eee8b782454283378ceb0955e70cf58c9b
unchartedworks <liang@unchartedworks.com>
2017-12-20 10:11:55 +0100

    initial commit

---
+++ AppIconGenerator/ImageMetaData.swift
@@ -0,0 +1,9 @@
+//
+//  ImageMetaData.swift
+//  AppIconSetGenerator
+//
+//  Created by Liang on 20/12/2017.
+//  Copyright © 2017 unhcartedworks.com. All rights reserved.
+//
+
+import Foundation
---
+++ AppIconGenerator/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  AppIconGenerator
+//
+//  Created by Liang on 18/12/2017.
+//  Copyright © 2017 unhcartedworks.com. All rights reserved.
+//
+
+import Cocoa
+
+class MainView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
